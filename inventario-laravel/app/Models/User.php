7d8546430eac5f77a770c2ae3f91b803b57cd60c
CharlottePlata <BCPR31912393@soy.sena.edu.co>
2026-07-21 16:54:24 -0500

    .

--- inventario-laravel/app/Models/User.php
+++ inventario-laravel/app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\factories\HasFactory;
-use Illuminate\Fundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -39,11 +39,11 @@ class User extends Authenticatable
      * 
      * @var array<string, string>
      */
-    protected function cast ():array
+    protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
-            'paswword' => 'hashed',
+            'password' => 'hashed',
         ];
     }
 
--- inventario-laravel/routes/web.php
+++ inventario-laravel/routes/web.php
@@ -1,38 +1,39 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illiminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\SubcategoriaController;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\UserController;
 
+
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/dashborad', function () {
+    return view('dashborad');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile',[ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile',[ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile',[ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['admin'])->group(function () {
+Route::middleware('admin')->group(function() {
     Route::resource('categorias', CategoriaController::class);
     Route::resource('subcategorias', SubcategoriaController::class);
     Route::resource('productos', ProductoController::class);
     Route::resource('usuarios', UserController::class);
 });
 
-Route::middleware(['coordinador'])->group(function () {
+Route::middleware('coordinador')->group(function() {
     Route::resource('usuarios', UserController::class)->except(['destroy']);
-    Route::resource('categorias', CategoriaController::class)->except(['destroy']);;
-    Route::resource('subcategorias', SubcategoriaController::class)->except(['destroy']);;
-    Route::resource('productos', ProductoController::class)->except(['destroy']);;
+    Route::resource('categorias', CategoriaController::class)->except(['destroy']);
+    Route::resource('subcategorias', SubcategoriaController::class)->except(['destroy']);
+    Route::resource('productos', ProductoController::class)->except(['destroy']);
 });
 
 require __DIR__.'/auth.php';
